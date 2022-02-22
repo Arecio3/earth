6af1c07407a87519047da6942cc0e5c4f18c4d92
Arecio Canton <devmain@Arecios-iMac.local>
2022-02-22 16:07:17 -0500

    you can control earth

--- src/Components/Earth/index.js
+++ src/Components/Earth/index.js
@@ -1,29 +1,47 @@
-import React from 'react'
-import EarthDayMap from '../../assets/textures/8k_earth_daymap.jpg'
-import EarthNormalMap from '../../assets/textures/8k_earth_normal_map.jpg'
-import EarthSpecularMap from '../../assets/textures/8k_earth_specular_map.jpg'
-import EarthCloudsMap from '../../assets/textures/8k_earth_clouds.jpg'
-import { useLoader } from '@react-three/fiber'
-import { TextureLoader } from 'three'
+import React from "react";
+
+// Camera
+import { OrbitControls } from "@react-three/drei";
+
+// Textures
+import EarthDayMap from "../../assets/textures/8k_earth_daymap.jpg";
+import EarthNormalMap from "../../assets/textures/8k_earth_normal_map.jpg";
+import EarthSpecularMap from "../../assets/textures/8k_earth_specular_map.jpg";
+import EarthCloudsMap from "../../assets/textures/8k_earth_clouds.jpg";
+// Texture Loader
+import { useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three";
 
 const Earth = (props) => {
-    // Loads Textures
-    const [colorMap, normalMap, specularMap, cloudsMap] = useLoader(TextureLoader, [EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap]);
+  // Loads Textures
+  const [colorMap, normalMap, specularMap, cloudsMap] = useLoader(
+    TextureLoader,
+    [EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap]
+  );
   return (
-      <>
+    <>
       {/* Ambient Lighting fills every spot of 3D World (without light you cant see shit)*/}
-      <ambientLight intensity={1}/>
-       {/* Whatever Geometric Shape */}
-        <mesh>
-            {/* args are the properties that get passed through once instantiated */}
-            <sphereGeometry args={[1, 32, 32]}/>
-            {/* Material (Colors, textures) */}
-            <meshPhongMaterial specularMap={specularMap}/>
-            {/* Physical Base Rendering (renders dependent on the physical material being reflected) */}
-            <meshStandardMaterial map={colorMap} normalMap={normalMap}/>
-        </mesh>
+      <ambientLight intensity={1} />
+      {/* Whatever Geometric Shape */}
+      <mesh >
+        {/* args are the properties that get passed through once instantiated */}
+        <sphereGeometry args={[1, 32, 32]} />
+        {/* Material (Colors, textures) */}
+        <meshPhongMaterial specularMap={specularMap} />
+        {/* Physical Base Rendering (renders dependent on the physical material being reflected) */}
+        <meshStandardMaterial map={colorMap} normalMap={normalMap} />
+        {/* Control over Camera Movement */}
+        <OrbitControls
+          enableZoom={true}
+          enablePan={true}
+          enableRotate={true}
+          zoomSpeed={0.6}
+          panSpeed={0.5}
+          rotateSpeed={0.4}
+        />
+      </mesh>
     </>
-  )
-}
+  );
+};
 
-export default Earth
+export default Earth;
